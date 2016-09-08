adaa3c6169bfa4b798477600a57fa611e554a27a
rainbow82 <shannonbeach1982@gmail.com>
2016-09-07 21:02:54 -0400

    Basic Website
    files needed to create a basic .net mvc website

---
+++ Asp.Net MVC Workshop Website/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Asp.Net_MVC_Workshop_Website
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Asp.Net MVC Workshop Website/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Asp.Net_MVC_Workshop_Website.MvcApplication" Language="C#" %>
